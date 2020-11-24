3ae90f7b617cbe1e0fa35e3b8342e1a205dee8c5
AayushKucheria <aayush.kucheria@gmail.com>
2020-11-24 13:03:53 +0200

    Tree working. Added sensors for add/remove. Add works, remove has bugs. Added json

--- tabs_api.js
+++ tabs_api.js
@@ -2,304 +2,131 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 // https://typeofnan.dev/an-easy-way-to-build-a-tree-with-object-references/
-tabs = {};
-tabIds = [];
-focusedWindowId = undefined;
-currentWindowId = undefined;
+
+let data = []; // tree of tabs as objects
+let root = [];
+let idMapping = [];
+
+
 function bootStrap() {
-  chrome.windows.getCurrent(function(currentWindow) {
-    currentWindowId = currentWindow.id;
-    chrome.windows.getLastFocused(function(focusedWindow) {
-      focusedWindowId = focusedWindow.id;
-      loadWindowList();
-    });
-  });
+  loadWindowList();
 }
 function isInt(i) {
   return (typeof i == "number") && !(i % 1) && !isNaN(i);
 }
 
+function printRoot() {
+  root.forEach((element) => console.log(element.id, element.parentId, element.children));
+
+}
+// Load tree from scratch
 function loadWindowList() {
+  console.log("Reloading")
+  data = [];
+  // Get windows + tabs data from chrome api
   chrome.windows.getAll({ populate: true }, function(windowList) {
-    let data = [];
-
-    // if(data.length != 0) {
-    //   // console.log("Data: ", data);
-    //   data = [];
-    // };
-
+    // windowList[0].tabs.map(n => console.log(n.id, "has parent" , n.openerTabId))
+    // For each tab in each window
+    // Add the tab's id, parent's id, and set it's children as empty (for now)
     for(var i=0; i < windowList.length; i++) {
       for (var j=0; j < windowList[i].tabs.length; j++) {
-        console.log(windowList[i].tabs[j].openerTabId);
-
         data.push({ "id": windowList[i].tabs[j].id,
                     "parentId": windowList[i].tabs[j].openerTabId,
                     "children": [] });
       };
     };
 
-
-    // Printing the whole linear array of tab objects h
-    // Making an ID-to-Index Map
-    const idMapping = data.reduce((acc, elem, index) => {
+    /* Making an ID-to-Index Map (for ease of access)
+      Syntax: [tab_id: index_in_data]
+      Example: [5648: 0, 5710: 4, 5736: 2, ..., 5788: 6]
+    */
+    idMapping = data.reduce((acc, elem, index) => {
       acc[elem.id] = index;
       return acc;
     }, {});
-    // console.log(idMapping);
-    // {id: index}
 
-
-    let root = [];
+    console.log(idMapping[0])
+    // For each tab, if it's a root (i.e. it doesn't have a parent),
+    // Then add it to the list of roots
+    // Else, Find its parent and insert the tab in the parent's children list.
+    root = [];
     data.forEach(element => {
-      // Handle the root elements
-     // console.log(element.id, element.parentId, element.children);
      if(element.parentId === undefined) {
        root.push(element);
-      return;
      }
      else {
       // Use our mapping to locate the parent element in our data array
+      // And add this tab as it's
         const parentElement = data[idMapping[element.parentId]];
-        // parentElement.children = [...(parentElement.children), element];
         parentElement.children.push(element);
-
-        console.log("Hello world");
       };
     });
 
-    //for(i in root) {
-    //  console.log(i);
-    //};
-    // root.forEach((element) => console.log(element.id, element.parentId, element.children));
-    // console.log(root);
-
+    // Print roots of each tree
+    printRoot();
  });
 };
-// function updateTabData(id) {
-//   var retval = {
-//     url: document.getElementById('url_' + id).value,
-//     selected: document.getElementById('selected_' + id).value ? true : false
-//   }
-//   return retval;
-// }
-// function updateTab(id){
-//   try {
-//     chrome.tabs.update(id, updateTabData(id));
-//   } catch (e) {
-//     alert(e);
-//   }
-// }
-// function moveTabData(id) {
-//   return {
-//     'index': parseInt(document.getElementById('index_' + id).value),
-//     'windowId': parseInt(document.getElementById('windowId_' + id).value)
-//   }
-// }
-// function moveTab(id) {
-//   try {
-//     chrome.tabs.move(id, moveTabData(id));
-//   } catch (e) {
-//     alert(e);
-//   }
-// }
-// function createTabData(id) {
-//   return {
-//     'index': parseInt(document.getElementById('index_' + id).value),
-//     'windowId': parseInt(document.getElementById('windowId_' + id).value),
-//     'index': parseInt(document.getElementById('index_' + id).value),
-//     'url': document.getElementById('url_' + id).value,
-//     'selected': document.getElementById('selected_' + id).value ? true : false
-//   }
-// }
-// function createTab() {
-//   var args = createTabData('new')
-//   if (!isInt(args.windowId))
-//     delete args.windowId;
-//   if (!isInt(args.index))
-//     delete args.index;
-//   try {
-//     chrome.tabs.create(args);
-//   } catch (e) {
-//     alert(e);
-//   }
-// }
-// function updateAll() {
-//   try {
-//     for (var i = 0; i < tabIds.length; i++) {
-//       chrome.tabs.update(tabIds[i], updateTabData(tabIds[i]));
-//     }
-//   } catch(e) {
-//     alert(e);
-//   }
-// }
-// function moveAll() {
-//   appendToLog('moving all');
-//   try {
-//     for (var i = 0; i < tabIds.length; i++) {
-//       chrome.tabs.move(tabIds[i], moveTabData(tabIds[i]));
-//     }
-//   } catch(e) {
-//     alert(e);
-//   }
-// }
-// function removeTab(tabId) {
-//   try {
-//     chrome.tabs.remove(tabId, function() {
-//       appendToLog('tab: ' + tabId + ' removed.');
-//     });
-//   } catch (e) {
-//     alert(e);
-//   }
-// }
-// function appendToLog(logLine) {
-//   document.getElementById('log')
-//       .appendChild(document.createElement('div'))
-//       .innerText = "> " + logLine;
-// }
-// function clearLog() {
-//   document.getElementById('log').innerText = '';
-// }
-chrome.windows.onCreated.addListener(function(createInfo) {
-  // appendToLog('windows.onCreated -- window: ' + createInfo.id);
-  loadWindowList();
-});
-// chrome.windows.onFocusChanged.addListener(function(windowId) {
-// //  focusedWindowId = windowId;
-// //   appendToLog('windows.onFocusChanged -- window: ' + windowId);
-//   loadWindowList();
-// });
-chrome.windows.onRemoved.addListener(function(windowId) {
-//   appendToLog('windows.onRemoved -- window: ' + windowId);
-  loadWindowList();
-});
+
+function addNewTab(tab) {
+
+  tabObj = { "id": tab.id,
+              "parentId": tab.openerTabId,
+              "children": [] }
+  data.push(tabObj);
+
+  // TODO: WRONG! Add to (and understand) idMapping
+
+  idMapping[tabObj.id] = data.indexOf(tabObj);
+
+  if(tabObj.parentId === undefined) {
+    root.push(tabObj)
+  }
+  else {
+    const parentElement = data[idMapping[tabObj.parentId]];
+    parentElement.children.push(tabObj);
+  };
+  console.log("Added new tab")
+  printRoot();
+};
+
 chrome.tabs.onCreated.addListener(function(tab) {
-  // appendToLog(
-  //     'tabs.onCreated -- window: ' + tab.windowId + ' tab: ' + tab.id +
-  //     ' title: ' + tab.title + ' index ' + tab.index + ' url ' + tab.url);
-  loadWindowList();
+  addNewTab(tab);
+
 });
-// chrome.tabs.onAttached.addListener(function(tabId, props) {
-// //   appendToLog(
-// //       'tabs.onAttached -- window: ' + props.newWindowId + ' tab: ' + tabId +
-// //       ' index ' + props.newPosition);
-//   loadWindowList();
-// });
-// chrome.tabs.onMoved.addListener(function(tabId, props) {
-// //   appendToLog(
-// //       'tabs.onMoved -- window: ' + props.windowId + ' tab: ' + tabId +
-// //       ' from ' + props.fromIndex + ' to ' +  props.toIndex);
-//   loadWindowList();
-// });
+
+// TODO: For later
 // function refreshTab(tabId) {
 //   chrome.tabs.get(tabId, function(tab) {
-//     var input = new JsExprContext(tab);
-//     var output = document.getElementById('tab_' + tab.id);
-//     jstProcess(input, output);
-//     appendToLog('tab refreshed -- tabId: ' + tab.id + ' url: ' + tab.url);
 //   });
 // }
 // chrome.tabs.onUpdated.addListener(function(tabId, props) {
-//   appendToLog(
-//       'tabs.onUpdated -- tab: ' + tabId + ' status ' + props.status +
-//       ' url ' + props.url);
-  // refreshTab(tabId);
+//   refreshTab(tabId);
 // });
-// chrome.tabs.onDetached.addListener(function(tabId, props) {
-// //   appendToLog(
-// //       'tabs.onDetached -- window: ' + props.oldWindowId + ' tab: ' + tabId +
-// //       ' index ' + props.oldPosition);
-//   loadWindowList();
-// });
-// chrome.tabs.onSelectionChanged.addListener(function(tabId, props) {
-// //   appendToLog(
-// //       'tabs.onSelectionChanged -- window: ' + props.windowId + ' tab: ' +
-// //       tabId);
-//   loadWindowList();
-//});
+
+function removeTab(tabId) {
+
+  // Removing tab from data and idMapping
+  // removedTab = data.find(elem => elem.id == tabId);
+  indexInData = idMapping[tabId];
+  console.log(indexInData)
+  removedTab = data.splice(indexInData, 1)
+  // idMapping.splice(tabId, 1);
+  delete idMapping[tabId];
+
+  // Removing from parent's children listS
+  console.log("Parent id = ", removedTab.parentId)
+  parentIndexInData = idMapping[removedTab.parentId]
+  console.log("Parent Index = ", parentIndexInData)
+  data[parentIndexInData].children = data[parentIndexInData].children.filter(child => child.id != removedTab.parentId);
+
+  console.log("Removed 1 tab")
+  printRoot();
+}
 chrome.tabs.onRemoved.addListener(function(tabId) {
 //   appendToLog('tabs.onRemoved -- tab: ' + tabId);
-  loadWindowList();
+    removeTab(tabId);
 });
-// function createWindow() {
-//   var args = {
-//     'left': parseInt(document.getElementById('new_window_left').value),
-//     'top': parseInt(document.getElementById('new_window_top').value),
-//     'width': parseInt(document.getElementById('new_window_width').value),
-//     'height': parseInt(document.getElementById('new_window_height').value),
-//     'url': document.getElementById('new_window_url').value
-//   }
-//   if (!isInt(args.left))
-//     delete args.left;
-//   if (!isInt(args.top))
-//     delete args.top;
-//   if (!isInt(args.width))
-//     delete args.width;
-//   if (!isInt(args.height))
-//     delete args.height;
-//   if (!args.url)
-//     delete args.url;
-//   try {
-//     chrome.windows.create(args);
-//   } catch(e) {
-//     alert(e);
-//   }
-// }
-// function refreshWindow(windowId) {
-//   chrome.windows.get(windowId, function(window) {
-//     chrome.tabs.getAllInWindow(window.id, function(tabList) {
-//       window.tabs = tabList;
-//       var input = new JsExprContext(window);
-//       var output = document.getElementById('window_' + window.id);
-//       jstProcess(input, output);
-//       appendToLog(
-//           'window refreshed -- windowId: ' + window.id + ' tab count:' +
-//           window.tabs.length);
-//     });
-//   });
-// }
-// function updateWindowData(id) {
-//   var retval = {
-//     left: parseInt(document.getElementById('left_' + id).value),
-//     top: parseInt(document.getElementById('top_' + id).value),
-//     width: parseInt(document.getElementById('width_' + id).value),
-//     height: parseInt(document.getElementById('height_' + id).value)
-//   }
-//   if (!isInt(retval.left))
-//     delete retval.left;
-//   if (!isInt(retval.top))
-//     delete retval.top;
-//   if (!isInt(retval.width))
-//     delete retval.width;
-//   if (!isInt(retval.height))
-//     delete retval.height;
-//   return retval;
-// }
-// function updateWindow(id){
-//   try {
-//     chrome.windows.update(id, updateWindowData(id));
-//   } catch (e) {
-//     alert(e);
-//   }
-// }
-// function removeWindow(windowId) {
-//   try {
-//     chrome.windows.remove(windowId, function() {
-//       appendToLog('window: ' + windowId + ' removed.');
-//     });
-//   } catch (e) {
-//     alert(e);
-//   }
-// }
-// function refreshSelectedTab(windowId) {
-//   chrome.tabs.query({active: true, currentWindow: true} function(tabs) {
-//     var input = new JsExprContext(tabs[0]);
-//     var output = document.getElementById('tab_' + tabs[0].id);
-//     jstProcess(input, output);
-//     appendToLog(
-//         'selected tab refreshed -- tabId: ' + tabs[0].id +
-//         ' url:' + tabs[0].url);
-//   });
-// }
+
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('button').addEventListener('click', loadWindowList);
 
