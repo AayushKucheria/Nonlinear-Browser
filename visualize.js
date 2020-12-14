0b2a2534bd64b3a2e68de2adfc13439fa647e41f
AayushKucheria <aayush.kucheria@gmail.com>
2020-12-14 12:29:49 +0200

    on Click added

--- visualize.js
+++ visualize.js
@@ -1,31 +1,27 @@
 
-
+// The whole svg element
 const svg = d3.select('svg')
 const width = document.body.clientWidth;
 const height = document.body.clientHeight;
-const margin = { top: 0, right: 50, bottom: 0, left: 75};
+const margin = { top: 20, right: 50, bottom: 30, left: 75};
 const innerWidth = width - margin.left - margin.right;
 const innerHeight = height - margin.top - margin.bottom;
 const tabWidth = 120;
 const tabHeight = 40;
-
 const treeLayout = d3.tree().size([height, width]);
 
-
-
+// Our groups?
 const g = svg
     .attr('width', width)
     .attr('height', height)
-    .attr("text-anchor", "middle")
+    // .attr("text-anchor", "middle")
   .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`)
-    .on('click', d => alert(d.toElement.__data__.data.url));
-    // .selectAll(".tick text")
-    // .call(wrap,15);
-// g.selectAll('g').on("click", d => {
-//   console.log(d)
-//   d.data.url
-// })
+    .on('click', d => {
+      chrome.tabs.update(d.toElement.__data__.data.id, {
+        active: true
+      });
+    })
 
 let transform;
 
@@ -38,9 +34,9 @@ function visualizeTree(localRoot) {
 
   const root = d3.hierarchy(localRoot);
   const links = treeLayout(root).links();
-  const linkPathGenerator = d3.linkVertical()
-    .x(d => 2 * d.x)
-    .y(d => d.y)
+  const linkPathGenerator = d3.linkHorizontal()
+    .x(d => d.y)
+    .y(d => d.x)
 
   // var tabs = g.selectAll("g").data(root.descendants());
 
@@ -61,21 +57,18 @@ function visualizeTree(localRoot) {
     .join(
       enter => {
         enter.append('text')
-          .text(d => d.data.title)
+          .text(d => d.data.id)
           .attr('dy', '0.32em')
-          .attr('x', d => 2 * d.x)
-          .attr('y', d => d.y + (tabHeight/2))
-          .style('fill', 'red')
-          .attr('width', tabWidth);
-          // .call(wrap, tabWidth)
+          .attr('x', d => d.y)
+          .attr('y', d => d.x)
+          .style('fill', 'red');
       },
       update => {
         update
           .transition()
           .duration(750)
-          .attr('x', d => 2 * d.x)
-          .attr('y', d => d.y + (tabHeight/2))
-          // .call(wrap, tabWidth);
+          .attr('x', d => d.y)
+          .attr('y', d => d.x);
       },
       exit => exit.remove()
     );
@@ -86,54 +79,67 @@ function visualizeTree(localRoot) {
           enter.append('rect')
             .attr('width', tabWidth)
             .attr('height', tabHeight)
-            .attr('x', d => (2 * d.x) - tabWidth/2)
-            .attr('y', d => d.y)
+            .attr('x', d => d.y)
+            .attr('y', d => d.x)
             .attr('fill-opacity', 0.2)
-
         },
         update => {
           update
             .transition()
             .duration(750)
-            .attr('x', d => (2 * d.x) - tabWidth/2)
-            .attr('y', d => d.y);
+            .attr('x', d => d.y)
+            .attr('y', d => d.x);
         },
         exit => exit.remove()
       );
 }
 
-function wrap(text, width, d) {
-    text.each(function () {
-        var text = d3.select(this),
-            words = text.text().split(/\s+/).reverse(),
-            word,
-            line = [],
-            lineNumber = 0,
-            lineHeight = 1.1, // ems
-            x = text.attr("x"),
-            y = text.attr("y"),
-            dy = parseFloat(text.attr("dy")),
-            tspan = text.text(null)
-                        .append("tspan")
-                        .attr("x", x)
-                        .attr("y", y)
-                        .attr("dy", dy + "em");
-        while (word = words.pop()) {
-            line.push(word);
-            tspan.text(line.join(" "));
-            if (tspan.node().getComputedTextLength() > width) {
-                line.pop();
-                tspan.text(line.join(" "));
-                line = [word];
-                tspan = text.append("tspan")
-                            .attr("x", x)
-                            .attr("y", y)
-                            .attr("dy", ++lineNumber * lineHeight + dy + "em")
-                            .text(word);
-            }
-        }
-    });
+
+
+// Helper functions for collapsing and expanding nodes
+
+function collapse(d) {
+  if(d.children) {
+    d._children = d.children;
+    d._children.foreach(collapse);
+    d.children = null;
+  }
 }
+
+function expand(d) {
+  if(d._children) {
+    d.children = d._children;
+    d.children.foreach(expand);
+    d._children = null;
+  }
+}
+
+// Function to center node when clicked/dropped so node doesn't get lost when collapsing/moving around with a large amount of children
+
+// function centerNode(source) {
+//   scale =zoomListener.scale();
+//   x = -source.y0;
+// }
+
+function toggleChildren(d) {
+  if(d.children) {
+    d._children = d.children;
+    d.children = null;
+  }
+  else if(d._children) {
+    d.children = d._children;
+    d._children = null;
+  }
+  return d;
+}
+
+function click(d) {
+  if(d3.event.defaultPrevented) return;
+  d = toggleChildren(d);
+  update(d);
+  centerNode(d);
+}
+
 
 
 // const svg = d3.select('svg');
