48ad4a34d59e7b5c5d6e2dd32b36211d0cfd05a7
AayushKucheria <aayush.kucheria@gmail.com>
2021-01-08 15:18:48 +0200

    connected firebase half

--- firebase.js
+++ firebase.js
@@ -1,15 +1,1 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyBcXi7lHnFZPpiQJEwIvs9u_gp38zst1mQ",
-  databaseURL: "https://nonlinear-browser-default-rtdb.firebaseio.com",
-  authDomain: "nonlinear-browser.firebaseapp.com",
-  projectId: "nonlinear-browser",
-  storageBucket: "nonlinear-browser.appspot.com",
-  messagingSenderId: "693229853662",
-  appId: "1:693229853662:web:cc17084511b58095841b4f",
-  measurementId: "G-76DQZXB7F5"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
 
-const db= firebase.firestore();
